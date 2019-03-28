50de4fdefc64a5033b821069846f4072cbfd04c0
Alberto <albm@itu.dk>
2019-03-28 22:52:28 +0100

    deleted some prints and comments

--- main.py
+++ main.py
@@ -13,15 +13,10 @@ def main():
     pd.set_option('display.width', 300)
 
     apriori_result = apriori(shopping_data, 0.15, 0.3)
-    # apriori_result = apriori_result.sort_values(by=['confidence'], ascending=[True])
     apriori_result = apriori_result.sort_values(by=['ir'], ascending=[True])
-    print(apriori_result)
 
     plt.show(apriori_result.query('ir > 0.0')
              .plot(kind='bar', x='rule', y=['ir', 'kulczynski'], rot=45, fontsize=6))
-
-    # plt.show(apriori_result.query('ir > 0.0')
-    #          .plot(kind='bar', x='rule', y=['support', 'confidence'], rot=45, fontsize=6))
 
 
 if __name__ == "__main__":
