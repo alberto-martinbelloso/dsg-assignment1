57dff7c668626158063e2d9690bbc62563e943ea
Alberto <albm@itu.dk>
2019-03-28 22:49:57 +0100

    last apriori commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from algorithms.apriori import apriori
 from data_reader import get_data
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -7,8 +9,19 @@ def main():
     shopping_data = get_data("shopping.json")
     telecom_data = get_data("telecom_churn.csv")
 
-    apriori_result = apriori(shopping_data, 0.3, 0.5)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', 300)
+
+    apriori_result = apriori(shopping_data, 0.15, 0.3)
+    # apriori_result = apriori_result.sort_values(by=['confidence'], ascending=[True])
+    apriori_result = apriori_result.sort_values(by=['ir'], ascending=[True])
     print(apriori_result)
+
+    plt.show(apriori_result.query('ir > 0.0')
+             .plot(kind='bar', x='rule', y=['ir', 'kulczynski'], rot=45, fontsize=6))
+
+    # plt.show(apriori_result.query('ir > 0.0')
+    #          .plot(kind='bar', x='rule', y=['support', 'confidence'], rot=45, fontsize=6))
 
 
 if __name__ == "__main__":
